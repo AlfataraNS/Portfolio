1f9ef78b1962da222a7498882281dfe48d36a77b
Alfatara Nurus Sa'adah <alfatara.ns@gmail.com>
2022-06-22 03:23:10 -0700

    adding files

---
+++ script.js
@@ -0,0 +1,57 @@
+let btnHome = () => {
+    document.querySelector('#home').style.display = 'flex';
+    document.querySelector('#about').style.display = 'none';
+    document.querySelector('#portfolio').style.display = 'none';
+    document.querySelector('#blog').style.display = 'none';
+    document.querySelector('#team').style.display = 'none';
+    document.querySelector('#contact').style.display = 'none';
+}
+
+let btnAbout = () => {
+    document.querySelector('#about').style.display = 'block';
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#portfolio').style.display = 'none';
+    document.querySelector('#blog').style.display = 'none';
+    document.querySelector('#team').style.display = 'none';
+    document.querySelector('#contact').style.display = 'none';
+}
+
+let btnPortfolio = () => {
+    document.querySelector('#portfolio').style.display = 'block';
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#about').style.display = 'none';
+    document.querySelector('#blog').style.display = 'none';
+    document.querySelector('#team').style.display = 'none';
+    document.querySelector('#contact').style.display = 'none';
+}
+
+let btnBlog = () => {
+    document.querySelector('#blog').style.display = 'block';
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#portfolio').style.display = 'none';
+    document.querySelector('#about').style.display = 'none';
+    document.querySelector('#team').style.display = 'none';
+    document.querySelector('#contact').style.display = 'none';
+}
+
+let btnTeam = () => {
+    document.querySelector('#team').style.display = 'block';
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#portfolio').style.display = 'none';
+    document.querySelector('#blog').style.display = 'none';
+    document.querySelector('#about').style.display = 'none';
+    document.querySelector('#contact').style.display = 'none';
+}
+
+let btnContact = () => {
+    document.querySelector('#contact').style.display = 'block';
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#portfolio').style.display = 'none';
+    document.querySelector('#blog').style.display = 'none';
+    document.querySelector('#team').style.display = 'none';
+    document.querySelector('#about').style.display = 'none';
+}
+
+// let btnContactLeave = () => {
+//     document.querySelector('#contact').style.display = 'none';
+// }
